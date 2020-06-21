1c3ba37dde002c7fe323b59a96d2f0e887cf427a
Gibin T George <gibintgeorge@hotmail.com>
2020-06-22 01:00:52 +0530

    Basic routes without DBs

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect to database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+//Define routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/post"));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
