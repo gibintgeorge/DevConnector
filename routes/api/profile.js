af2ee02450c3d0f35072e27d7ada82cc4097856b
Gibin T George <gibintgeorge@hotmail.com>
2020-06-23 01:24:40 +0530

    Delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -141,4 +141,24 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+//@route    DELETE api/profile
+//@desc     Delete  profile, user,posts
+//@access   Public
+router.delete("/", auth, async (req, res) => {
+  try {
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // remove post
+
+    // remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
 module.exports = router;
